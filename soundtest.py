681811a7a51a174d46c27adde3e758c097093f46
JaiPipatt <j.jaijai108@gmail.com>
2025-04-13 00:39:19 +0700

    .

--- soundtest.py
+++ soundtest.py
@@ -1,7 +1,10 @@
 import sounddevice as sd
 from playsound_v1 import mix
 sound = mix(['sound\summer\RADISH.wav'])
-print(sound)
-while True:
-    sd.play(sound[0], samplerate=sound[1])
-    sd.wait()
+print('ctrl+c to stop')
+try:
+    while True:
+        sd.play(sound[0], samplerate=sound[1])
+        sd.wait()   
+except KeyboardInterrupt:
+    print("yood")
