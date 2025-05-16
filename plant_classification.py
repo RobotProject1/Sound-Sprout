c38fbb7632805b3185336aea7607cfab5858fbd3
FunSnakePKP <pakapolaew@gmail.com>
2025-05-16 13:36:05 +0700

    adjust

--- plant_classification.py
+++ plant_classification.py
@@ -30,33 +30,25 @@ import busio
 from adafruit_ads1x15.ads1115 import ADS1115
 from adafruit_ads1x15.analog_in import AnalogIn
 
-# I2C setup
 i2c = busio.I2C(board.SCL, board.SDA)
-
-# Initialize both ADS1115 devices with their I2C addresses
-# ads1 = ADS1115(i2c, address=0x48)
+ads1 = ADS1115(i2c, address=0x48)
 ads2 = ADS1115(i2c, address=0x49)
 
-# Channel mapping
-# pin1 = [1, 2, 3]  # A0, A1, A2 on ADS1
-pin2 = [1, 2, 3]  # A0, A1, A2 on ADS2
+pin1 = [1, 2, 3]
+pin2 = [1, 2, 3]
 
-# Voltage-ID mapping
-id2v_dict = [(4.78, 1), (4.35, 2), (3.98, 3), (3.64, 4), (3.33, 5), (3.03, 6),
-             (2.67, 7), (2.38, 8), (2.13, 9), (1.79, 10), (1.56, 11), (1.09, 12)]
+id2v_dict = [(4.78, 1), (4.35, 2), (3.98, 3), (3.64, 4), (3.33, 5), (3.03, 6), (2.67, 7), (2.38, 8), (2.13, 9), (1.79, 10), (1.56, 11), (1.09, 12)]
 
-# Read voltages from both chips
 def read_v():
     v_list = []
-    # for i in pin1:
-    #     chan = AnalogIn(ads1, getattr(ADS1115, f"P{i}"))
-    #     v_list.append(chan.voltage)
+    for pin in pin1:
+         chan = AnalogIn(ads1, pin)
+         v_list.append(chan.voltage)
     for pin in pin2:
         chan = AnalogIn(ads2, pin)
         v_list.append(chan.voltage)
     return v_list
 
-# Match voltage to closest known ID
 def read_id(v_list):
     id_list = []
     for v in v_list:
@@ -67,10 +59,3 @@ def read_id(v_list):
                 break
         id_list.append(matched_id)
     return id_list
-
-# Example usage
-if __name__ == "__main__":
-    voltages = read_v()
-    ids = read_id(voltages)
-    print("Voltages:", voltages)
-    print("IDs:", ids)
