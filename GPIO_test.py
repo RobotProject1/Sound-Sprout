2ea7270e06e9921f90a95bb113f0580ab1520561
FunSnakePKP <pakapolaew@gmail.com>
2025-05-17 16:57:32 +0700

    Update GPIO_test.py

--- GPIO_test.py
+++ GPIO_test.py
@@ -3,7 +3,7 @@ import Jetson.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)  # Disable warnings
 
-GPIO.setup(26, GPIO.IN)
+GPIO.setup(24, GPIO.IN)
 
 try:
     print("Waiting for rising edge on pin 24...")
