87ee942b7fa3e7e6c11cfdebb95e461d245d4483
FunSnakePKP <pakapolaew@gmail.com>
2025-05-17 17:35:35 +0700

    Update GPIO_test.py

--- GPIO_test.py
+++ GPIO_test.py
@@ -3,14 +3,14 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-GPIO.setup(35, GPIO.IN)
+GPIO.setup(7, GPIO.IN)
 
 try:
     while True:
-        if GPIO.input(35) == GPIO.HIGH:
-            print("Pin 35 is HIGH")
+        if GPIO.input(7) == GPIO.HIGH:
+            print("Pin 7 is HIGH")
         else:
-            print("Pin 35 is LOW")
+            print("Pin 7 is LOW")
         time.sleep(0.5)  # check twice a second
 finally:
     GPIO.cleanup()
