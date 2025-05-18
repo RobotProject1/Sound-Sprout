7dc83e16d9015f448b48cff5831d9bfdc8134ba0
FunSnakePKP <pakapolaew@gmail.com>
2025-05-18 15:48:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def run_script(script_name): # ex 'lighting_rainy.py'
     """
     script_path = os.path.join(os.path.dirname(__file__), script_name)
     print(f"Running script: {script_path}")
-    subprocess.run([sys.executable, script_path])
+    subprocess.Popen([sys.executable, script_path], shell=True)
 
 GPIO.setwarnings(False)
 GPIO.cleanup()
