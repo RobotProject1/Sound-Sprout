f5ec9bde0815ec668406e0a1a59eef397265267b
FunSnakePKP <pakapolaew@gmail.com>
2025-05-19 01:13:38 +0700

    adjust

---
+++ v_check_plant.py
@@ -0,0 +1,26 @@
+import board
+import busio
+from adafruit_ads1x15.ads1115 import ADS1115
+from adafruit_ads1x15.analog_in import AnalogIn
+import time
+
+# Initialize I2C and both ADS1115 ADCs
+i2c = busio.I2C(board.SCL, board.SDA)
+ads1 = ADS1115(i2c, address=0x48)
+ads2 = ADS1115(i2c, address=0x49)
+
+# Define pins to read (A0, A1, A2)
+pins = [0, 1, 2]
+
+print("Reading voltages from ADS1115 devices (press Ctrl+C to stop)\n")
+
+try:
+    while True:
+        for pin in pins:
+            chan1 = AnalogIn(ads1, pin)
+            chan2 = AnalogIn(ads2, pin)
+            print(f"ADS1 A{pin}: {chan1.voltage:.2f} V | ADS2 A{pin}: {chan2.voltage:.2f} V")
+        print("-" * 40)
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Exiting...")
