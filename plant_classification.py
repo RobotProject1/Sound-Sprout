cad54919ef986385435e6381fdfe0c2f677ef054
FunSnakePKP <pakapolaew@gmail.com>
2025-05-19 19:24:22 +0700

    adjust

--- plant_classification.py
+++ plant_classification.py
@@ -1,58 +1,21 @@
-# from adafruit_ads1x15.analog_in import AnalogIn
-# from shared_ads import ads1, ads2
-
-# pin1 = [0, 1, 2]
-# pin2 = [0, 1, 2]
-
-# # id2v_dict = [(4.78, 1), (4.35, 2), (3.98, 3), (3.64, 4), (3.33, 5), (3.03, 6), (2.67, 7), (2.38, 8), (2.13, 9), (1.79, 10), (1.56, 11), (1.09, 12)]
-# id2v_dict = [(4.10, 1), (3.87, 2), (3.67, 3), (3.59, 4), (3.20, 5), (2.98, 6), (2.64, 7), (2.18, 8), (1.99, 9), (1.75, 10), (1.50, 11), (1.15, 12)]
-
-# def read_v():
-#     v_list = []
-#     for pin in pin1:
-#          chan = AnalogIn(ads1, pin)
-#          v_list.append(chan.voltage)
-#     for pin in pin2:
-#         chan = AnalogIn(ads2, pin)
-#         v_list.append(chan.voltage)
-#     return v_list
-
-# def read_id(v_list):
-#     id_list = []
-#     valid_ids = set(range(1,13))
-#     for v in v_list:
-#         matched_id = 0
-#         for volt, id in id2v_dict:
-#             if abs(v - volt) < 0.05:
-#                 if id in valid_ids:
-#                     matched_id = id
-#                     break
-#         if matched_id != 0:
-#             id_list.append(matched_id)
-#     return id_list
-
 from adafruit_ads1x15.analog_in import AnalogIn
 from shared_ads import ads1, ads2
 
-# Only create these ONCE
-chan0 = AnalogIn(ads1, 0)
-chan1 = AnalogIn(ads1, 1)
-chan2 = AnalogIn(ads1, 2)
-chan3 = AnalogIn(ads2, 0)
-chan4 = AnalogIn(ads2, 1)
-chan5 = AnalogIn(ads2, 2)
+pin1 = [0, 1, 2]
+pin2 = [0, 1, 2]
 
-channels = [chan0, chan1, chan2, chan3, chan4, chan5]
-
-# Voltage-to-ID map
-id2v_dict = [
-    (4.10, 1), (3.87, 2), (3.67, 3), (3.59, 4),
-    (3.20, 5), (2.98, 6), (2.64, 7), (2.18, 8),
-    (1.99, 9), (1.75, 10), (1.50, 11), (1.15, 12)
-]
+# id2v_dict = [(4.78, 1), (4.35, 2), (3.98, 3), (3.64, 4), (3.33, 5), (3.03, 6), (2.67, 7), (2.38, 8), (2.13, 9), (1.79, 10), (1.56, 11), (1.09, 12)]
+id2v_dict = [(4.10, 1), (3.87, 2), (3.67, 3), (3.59, 4), (3.20, 5), (2.98, 6), (2.64, 7), (2.18, 8), (1.99, 9), (1.75, 10), (1.50, 11), (1.15, 12)]
 
 def read_v():
-    return [ch.voltage for ch in channels]
+    v_list = []
+    for pin in pin1:
+         chan = AnalogIn(ads1, pin)
+         v_list.append(chan.voltage)
+    for pin in pin2:
+        chan = AnalogIn(ads2, pin)
+        v_list.append(chan.voltage)
+    return v_list
 
 def read_id(v_list):
     id_list = []
--- shared_ads.py
+++ shared_ads.py
@@ -1,19 +1,8 @@
-# import board
-# import busio
-# from adafruit_ads1x15.ads1115 import ADS1115
-
-# i2c = busio.I2C(board.SCL, board.SDA)
-
-# ads1 = ADS1115(i2c, address=0x48)
-# ads2 = ADS1115(i2c, address=0x49)
-
-from adafruit_ads1x15.ads1115 import ADS1115
-from adafruit_ads1x15.ads1x15 import Mode
-import busio
 import board
+import busio
+from adafruit_ads1x15.ads1115 import ADS1115
 
 i2c = busio.I2C(board.SCL, board.SDA)
+
 ads1 = ADS1115(i2c, address=0x48)
 ads2 = ADS1115(i2c, address=0x49)
-ads2.gain = 4  # set gain to ±4.096V; try 2 for ±2.048V or 4 for ±1.024V
-ads2.mode = Mode.SINGLE  # Use SINGLE mode (slower, more precise)
