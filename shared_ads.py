e5e383049d772cdf6a9990c1916dcba802de9d69
FunSnakePKP <pakapolaew@gmail.com>
2025-05-19 18:32:57 +0700

    A H H H H H H H

--- shared_ads.py
+++ shared_ads.py
@@ -1,11 +1,20 @@
-from threading import Lock
-import board
+# from threading import Lock
+# import board
+# import busio
+# from adafruit_ads1x15.ads1115 import ADS1115
+
+# i2c = busio.I2C(board.SCL, board.SDA)
+
+# ads1 = ADS1115(i2c, address=0x48)
+# ads2 = ADS1115(i2c, address=0x49)
+
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.ads1x15 import Mode
 import busio
-from adafruit_ads1x15.ads1115 import ADS1115
+import board
+from adafruit_ads1x15.analog_in import AnalogIn
 
 i2c = busio.I2C(board.SCL, board.SDA)
-
-ads1 = ADS1115(i2c, address=0x48)
-ads2 = ADS1115(i2c, address=0x49)
-
-ads2_lock = Lock()
+ads2 = ADS.ADS1115(i2c)
+ads2.gain = 4  # set gain to ±4.096V; try 2 for ±2.048V or 4 for ±1.024V
+ads2.mode = Mode.SINGLE  # Use SINGLE mode (slower, more precise)
