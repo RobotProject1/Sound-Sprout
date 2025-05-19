e26c6968c8db3c388515e392228f95a4d6862fcc
FunSnakePKP <pakapolaew@gmail.com>
2025-05-19 14:27:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -105,13 +105,13 @@ if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)  
     GPIO.setup(onoff_pin, GPIO.IN)
     target_scripts = ['playsound.py','spring_sound.py','rain_sound.py','winter_sound.py']
+    run_script('playsound.py')
     while True:
         GPIO.wait_for_edge(onoff_pin, GPIO.RISING)
         print("ON button pressed. Starting system...")
         time.sleep(0.3)
         choose_season_thread = choose_season()
         choose_season_thread.start()
-        run_script('playsound.py')
         GPIO.wait_for_edge(onoff_pin, GPIO.RISING)
         print("OFF button pressed. Stopping system...")
         time.sleep(0.3)
