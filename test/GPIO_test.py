33a723899538a37a3b0870a2d0d47a5fe072f319
FunSnakePKP <pakapolaew@gmail.com>
2025-05-20 21:28:13 +0700

    Update GPIO_test.py

--- test/GPIO_test.py
+++ test/GPIO_test.py
@@ -3,14 +3,24 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-GPIO.setup(26, GPIO.IN)
+GPIO.setup(7, GPIO.IN) #button 1
+GPIO.setup(24, GPIO.IN) #button 2
+GPIO.setup(35, GPIO.IN) #button 3
 
 try:
     while True:
-        if GPIO.input(26) == GPIO.HIGH:
-            print("Pin 26 is HIGH")
+        if GPIO.input(7) == GPIO.HIGH:
+            print("Pin 7 is HIGH")
         else:
-            print("Pin 26 is LOW")
+            print("Pin 7 is LOW")
+        if GPIO.input(24) == GPIO.HIGH:
+            print("Pin 24 is HIGH")
+        else:
+            print("Pin 24 is LOW")
+        if GPIO.input(35) == GPIO.HIGH:
+            print("Pin 35 is HIGH")
+        else:
+            print("Pin 35 is LOW")
         time.sleep(0.5)  # check twice a second
 finally:
-    GPIO.cleanup()
+    GPIO.cleanup() 
