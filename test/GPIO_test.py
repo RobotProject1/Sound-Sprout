50a6d507e0ef6f079b31a25c7bb373dd03665534
FunSnakePKP <pakapolaew@gmail.com>
2025-05-21 01:06:38 +0700

    Update GPIO_test.py

--- test/GPIO_test.py
+++ test/GPIO_test.py
@@ -6,21 +6,26 @@ GPIO.setwarnings(False)
 GPIO.setup(7, GPIO.IN) #button 1
 GPIO.setup(24, GPIO.IN) #button 2
 GPIO.setup(35, GPIO.IN) #button 3
+GPIO.setup(26, GPIO.IN) #button 4
 
 try:
     while True:
         if GPIO.input(7) == GPIO.HIGH:
-            print("Pin 7 is HIGH")
+            print("Button 1 is HIGH")
         else:
-            print("Pin 7 is LOW")
+            print("Button 1 is LOW")
         if GPIO.input(24) == GPIO.HIGH:
-            print("Pin 24 is HIGH")
+            print("Button 2 is HIGH")
         else:
-            print("Pin 24 is LOW")
+            print("Button 2 is LOW")
         if GPIO.input(35) == GPIO.HIGH:
-            print("Pin 35 is HIGH")
+            print("Button 3 is HIGH")
         else:
-            print("Pin 35 is LOW")
+            print("Button 3 is LOW")
+        if GPIO.input(26) == GPIO.HIGH:
+            print("On/off Button is HIGH")
+        else:
+            print("On/off Button is LOW")
         time.sleep(0.5)  # check twice a second
 finally:
     GPIO.cleanup() 
