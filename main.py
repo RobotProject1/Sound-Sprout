2ac5f62b7696b6c9a8e3ccc86bcbf8dfe73813d9
FunSnakePKP <pakapolaew@gmail.com>
2025-05-22 17:07:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ process_lock = Lock()
 current_season = None
 audio_queue = Queue()  # Shared queue for audio paths
 
+GPIO.cleanup()
+
 # Initialize I2C and ADC
 try:
     i2c = busio.I2C(board.SCL, board.SDA)
